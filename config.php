6d21f8515cb0da0fad746023213e81e7ab19c5f9
JulesNyom <julesnyom@gmail.com>
2025-05-08 17:46:00 +0200

    Still failing to finish this auth, for an unknow reason i can not connect to mysql server with my app

--- config.php
+++ config.php
@@ -1,30 +1,23 @@
 <?php
-  $db_host = '127.0.0.1';
-  $db_user = 'root';
-  $db_password = '';
-  $db_db = '';
-  $db_port = 8889;
+// Using UNIX socket (recommended for MAMP)
+$db_host = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock'; // Socket path
+$db_user = '';
+$db_password = ''; // Use your actual password
+$db_db = '';
 
-  $mysqli = new mysqli(
+// Alternative TCP connection (use either this or socket)
+// $db_host = '127.0.0.1'; // Don't use 'localhost' for TCP
+// $db_port = 8889;        // MAMP's MySQL port
+
+$mysqli = new mysqli(
     $db_host,
     $db_user,
     $db_password,
-    $db_db,
-	$db_port
-  );
-	
-  if ($mysqli->connect_error) {
-    echo 'Errno: '.$mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
-    exit();
-  }
+    $db_db
+    // $db_port           // Uncomment for TCP
+);
 
-  echo 'Success: A proper connection to MySQL was made.';
-  echo '<br>';
-  echo 'Host information: '.$mysqli->host_info;
-  echo '<br>';
-  echo 'Protocol version: '.$mysqli->protocol_version;
-
-  $mysqli->close();
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 ?>
